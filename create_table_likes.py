cbc2e939017d27d01c25a39f9b47e7d574170c61
Max <maxrbx47@gmail.com>
2026-04-24 13:31:10 +0300

    table
    table for database

---
+++ create_table_likes.py
@@ -0,0 +1,11 @@
+import sqlite3
+connection = sqlite3.connect('sqlite.db', check_same_thread=False)
+cursor = connection.cursor()
+result = cursor.execute('''
+                    CREATE TABLE like (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        post_id INTEGER NOT NULL,
+                        user_id INTEGER NOT NULL );
+                    ''')
+connection.commit()
+connection.close()
